8246706d7f9dd00c865f00e3dda0fbf1c59990d6
unh4ppy <116509658+unh4ppy@users.noreply.github.com>
2022-12-18 16:57:46 +0300

    Converter

---
+++ Converter1/ConverterLib1.1/Converter.cs
@@ -0,0 +1,56 @@
+﻿using ConverterLib1._1.Values;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConverterLib1._1
+{
+    public class Converter
+    {
+        private List<string> _physicValues = new List<string>()
+        {
+            "Время",
+            "Масса",
+            "Скорость"
+        };
+        public List<string> GetPhysicValuesList()
+        {
+            return _physicValues;
+        }
+        public List<string> GetMeasureList(string physicValue)
+        {
+            switch (physicValue)
+            {
+                case "Время":
+                    _value = new Time();
+                    break;
+                case "Масса":
+                    _value = new Weight();
+                    break;
+                case "Скорость":
+                    _value = new Speed();
+                    break;
+            }
+            return _value.GetMeassureList();
+        }
+        private IValue _value;
+        private string _physicValue;
+        public double GetConvertedValue(string physicValue, double value, string from, string to)
+        {
+            _physicValue = physicValue;
+            switch (physicValue)
+            {
+                case "Время":
+                    _value = new Time();
+                    break;
+                case "Масса":
+                    _value = new Weight();
+                    break;
+                case "Скорость":
+                    _value = new Speed();
+                    break;
+            }
+            return _value.GetConvertedValue(value, from, to);
+        }
+    }
+}
---
+++ Converter1/ConverterLib1.1/Values/Speed.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConverterLib1._1.Values
+{
+    class Speed : IValue
+    {
+        private double _value;
+        private string _from;
+        private string _to;
+        private List<string> _measureList = new List<string>()
+        {
+            "Метры в секунду",
+            "Километры в час",
+        };
+        public double GetConvertedValue(double value, string from, string to)
+        {
+            _value = value;
+            ToSi();
+            ToRequired();
+            return _value;
+        }
+
+        public List<string> GetMeassureList()
+        {
+            return _measureList;
+        }
+
+        public void ToRequired()
+        {
+            switch (_to)
+            {
+                case "Метры в секунду":
+                    _value /= 1;
+                    break;
+                case "километры в час":
+                    _value *= 3.6;
+                    break;
+            }
+        }
+
+        public void ToSi()
+        {
+            switch (_from)
+            {
+                case "Метры в секунду":
+                    _value /= 1;
+                    break;
+                case "километры в час":
+                    _value /= 3.6;
+                    break;
+            }
+        }
+    }
+}
